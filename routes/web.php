8a4751913f625d884c482485902296183be2b379
norez-ali <norezali.dev@gmail.com>
2025-01-08 17:08:26 +0500

    include directive used

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Spatie\FlareClient\View;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/included', function () {
+    return View('pages.second');
 });
